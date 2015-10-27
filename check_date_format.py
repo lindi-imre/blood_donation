4277f85b70d0505f5ea7cd2e32d983d15ae7def6
Slezak Attila <attila.slezak@gmail.com>
2015-10-27 15:10:52 +0100

    Letrehoztam a check_date_format.py es a test_check_date_format.py fajlokat

--- check_date_format.py
+++ check_date_format.py
@@ -3,5 +3,16 @@ __author__ = 'Slezak Attila'
 
 from datetime import datetime
 
-class CheckDate:
+class CheckDateFormat(object):
     @staticmethod
+    def check_date_format(get_date):
+        date_parts = get_date.split(".")
+        print(date_parts)
+        if len(date_parts) == 3 and len(date_parts[0]) == 4:
+            for part in date_parts:
+                if not part.isdigit():
+                    print("Bad date format ! It should be YYYY.MM.DD !")
+                    return False
+            return True
+        print("Bad date format ! It should be YYYY.MM.DD !")
+        return False
--- test_check_date_format.py
+++ test_check_date_format.py
@@ -1,1 +1,19 @@
 __author__ = 'Slezak Attila'
+# -- coding: utf-8 --
+
+import unittest
+from check_date_format import CheckDateFormat
+
+class CheckDateFormatTest(unittest.TestCase):
+    def test_alphabet(self):
+        self.assertTrue(CheckDateFormat.check_date_format("Hihi"))
+
+    def test_correct(self):
+        self.assertTrue(CheckDateFormat.check_date_format("2016.02.01"))
+
+    # def test_fourteen(self):
+    #     self.assertEqual([2, 7], PrimeFactor.generate(14))
+
+
+if __name__ == '__main__':
+    unittest.main()
