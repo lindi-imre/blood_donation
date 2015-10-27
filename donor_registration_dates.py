e549c0d9063d8201ad3d8cc1a6a8532d578a6364
unknown <lindi.imre@gmail.com>
2015-10-27 12:17:53 +0100

    donor registration dates

--- donor_registration_dates.py
+++ donor_registration_dates.py
@@ -1,6 +1,12 @@
 __author__ = 'Imre'
 
+import datetime
+
 class donor_dates:
     @staticmethod
-    def get_info():
-        return input("Enter something!")
+    def get_date():
+        return datetime.datetime.strptime(input(" (year.month.day): "), "%Y.%m.%d.").date()
+
+    @staticmethod
+    def get_last_donation_date():
+        return datetime.datetime.strptime(input(" (year.month.day): "), "%Y.%m.%d.").date()
