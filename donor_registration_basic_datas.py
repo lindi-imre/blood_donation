1b1b86ccb98b1f6c4b12d8bcdca596be50a67832
Bagoly Peter <djpetyaaa@gmail.com>
2015-10-27 14:49:44 +0100

    Create the file and define today plan in a class

---
+++ donor_registration_basic_datas.py
@@ -0,0 +1,11 @@
+__author__ = 'Péter'
+
+class Person():
+    name = ""
+    weight = 0
+    gender = ""
+
+    def __init__(self,name,weight,gender):
+        self.name = name
+        self.weight = weight
+        self.weight = gender
