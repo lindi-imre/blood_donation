6473ac4b7741e07040fe23f5f846afd999189eee
Slezak Attila <attila.slezak@gmail.com>
2015-10-28 10:11:28 +0100

    Elkeszitettem a datum formatumot ellenorzo kodot es a unittest-et hozza

--- check_date_format.py
+++ check_date_format.py
@@ -8,9 +8,9 @@ class CheckDateFormat(object):
     def check_date_format(get_date):
         date_parts = get_date.split(".")
         print(date_parts)
-        if len(date_parts) == 3 and len(date_parts[0]) == 4:
+        if len(date_parts) == 3 and len(date_parts[0]) == 4 and len(date_parts[1]) == 2 and len(date_parts[2]) == 2:
             for part in date_parts:
-                if not part.isdigit():
+                if not part.isdigit() or int(date_parts[1]) > 12 or int(date_parts[2]) > 31:
                     print("Bad date format ! It should be YYYY.MM.DD !")
                     return False
             return True
--- test_check_date_format.py
+++ test_check_date_format.py
@@ -6,13 +6,22 @@ from check_date_format import CheckDateFormat
 
 class CheckDateFormatTest(unittest.TestCase):
     def test_alphabet(self):
-        self.assertTrue(CheckDateFormat.check_date_format("Hihi"))
+        self.assertFalse(CheckDateFormat.check_date_format("Hihi"))
+
+    def test_alphanumeric(self):
+        self.assertFalse(CheckDateFormat.check_date_format("2014agd"))
+
+    def test_too_much_number(self):
+        self.assertFalse(CheckDateFormat.check_date_format("2014.123.12"))
+
+    def test_too_big_month_number(self):
+        self.assertFalse(CheckDateFormat.check_date_format("2014.13.03"))
+
+    def test_too_big_day_number(self):
+        self.assertFalse(CheckDateFormat.check_date_format("2014.02.32"))
 
     def test_correct(self):
         self.assertTrue(CheckDateFormat.check_date_format("2016.02.01"))
-
-    # def test_fourteen(self):
-    #     self.assertEqual([2, 7], PrimeFactor.generate(14))
 
 
 if __name__ == '__main__':
