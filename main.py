fbc0ff7aaa21b79dde11f1481b3754be6b5fc913
Bagoly Peter <djpetyaaa@gmail.com>
2015-10-29 23:51:34 +0100

    Menüválasztás hibakezelése

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ print("<~~~~~~~~----------------------^^^^^^^----------------------~~~~~~~>")
 print("Welcome in the blood donor and event location register application!")
 print("Please select what would you like to register:\n1 - Donor registration\n2 - Donor event location registration")
 menu = input(">> ")
-if int(menu) == 1:
-    Person.donor_register_app()
+while not menu == 1 or not menu == 2:
+    if int(menu) == 1:
+        Person.donor_register_app()
+    elif int(menu) == 2:
+        Event.event_data()
+    else:
+        print("Please select a valid menu number!")
+        menu = input(">> ")
 
-elif int(menu) == 2:
-    Event.event_data()
+
