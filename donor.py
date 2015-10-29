41f72c7f3d206296a240b6e77c52cc9b2aa8060e
Bagoly Peter <djpetyaaa@gmail.com>
2015-10-29 17:35:25 +0100

    Updated validations file: validate & check: so close to finish

--- donor.py
+++ donor.py
@@ -1,11 +1,19 @@
 __author__ = 'Péter'
 
 from switch import Switch
+from donor_validations import Validations
 
 class Person():
-    name = Switch.general_data_inputer(["Name"])
-    weight = Switch.general_data_inputer(["Weight"])
-    gender = Switch.general_data_inputer(["Gender"])
+    @staticmethod
+    def donor_register_app():
+        name = Switch.general_data_inputer(["Name"])
+        weight = Switch.general_data_inputer(["Weight"])
+        gender = Switch.general_data_inputer(["Gender"])
+        uniqeid = Switch.general_data_inputer(["Uniqeid"])
 
-    uniqeid = Switch.general_data_inputer(["Uniqeid"])
+        sick = Switch.general_data_inputer(["Sick"])
+        phone_number = Switch.general_data_inputer(["Phone_number"])
+
+        email = Switch.general_data_inputer(["Email"])
+        hemoglobin = Validations.validate_hmg()
 
