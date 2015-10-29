f84ad32213376f99d39a9c9485e61d7d696d8745
Kozma Balazs <kozma.balazs1992@gmail.com>
2015-10-29 11:29:51 +0100

    nev korrekcio javitas

--- name_correct_form.py
+++ name_correct_form.py
@@ -6,12 +6,13 @@ class NameFormat(object):
     def name_corr_format(get_name):
         if type(get_name) != list:
             if len(get_name) > 1:
-                get_name = get_name[0].upper + get_name[1:].lower
+                get_name = get_name[0].upper() + get_name[1:].lower()
             else:
-                get_name = get_name.upper
+                get_name = get_name.upper()
         else:
             for i, one_name in enumerate(get_name):
                 if len(one_name) > 1:
-                    get_name[i] = one_name[0].upper + get_name[1:].lower
+                    get_name[i] = one_name[0].upper() + one_name[1:].lower()
                 else:
-                    get_name[i] = one_name.upper
+                    get_name[i] = one_name.upper()
+        return get_name
