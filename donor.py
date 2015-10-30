9bbc3e0ade32a89b905b4d93c4ad8241e75273d7
Bagoly Peter <djpetyaaa@gmail.com>
2015-10-30 11:08:47 +0100

    Esztétikai javítások

--- donor.py
+++ donor.py
@@ -6,15 +6,15 @@ from donor_validations import Validations
 class Person(object):
     @staticmethod
     def donor_register_app():
-        name = Switch.general_data_inputer(["Name"])
-        weight = Switch.general_data_inputer(["Weight"])
-        gender = Switch.general_data_inputer(["Gender"])
-        uniqeid = Switch.general_data_inputer(["Uniqeid"])
+        name = Switch.general_data_inputer(["Name", "Enter your full name"])
+        weight = Switch.general_data_inputer(["Weight", "Weight"])
+        gender = Switch.general_data_inputer(["Gender", "Gender"])
+        uniqeid = Switch.general_data_inputer(["Uniqeid", "Unique identifier number"])
 
-        sick = Switch.general_data_inputer(["Sick"])
-        phone_number = Switch.general_data_inputer(["Phone number"])
+        sick = Switch.general_data_inputer(["Sick", "Were you sick in the last month?"])
+        phone_number = Switch.general_data_inputer(["Phone number", "Mobile number"])
 
-        email = Switch.general_data_inputer(["Email"])
+        email = Switch.general_data_inputer(["Email", "E-mail adress"])
         hemoglobin = Validations.validate_hmg()
 
         print("\n" + "-" * 32 + "\n")
@@ -26,6 +26,6 @@ class Person(object):
         print("Were donor sick last month: ", sick)
         print("Phone number: ", phone_number)
         print("Email: ", email)
-        print("Haemoglobin level: ", hemoglobin)
+        print("Hemoglobin level: ", hemoglobin)
         print("\n" + "-" * 32)
 
