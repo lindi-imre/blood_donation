339ad86e9ff3ae2363daa807103281cdfce1efd7
Bagoly Peter <djpetyaaa@gmail.com>
2015-10-30 10:46:08 +0100

    Kilépés kezelése

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ print("<~~~~~~~~----------------------^^^^^^^----------------------~~~~~~~>")
 print("Welcome in the blood donor and event location register application!")
 print("Please select what would you like to register:\n1 - Donor registration\n2 - Donor event location registration")
 menu = input(">> ")
-while not menu == 1 or not menu == 2:
-    if int(menu) == 1:
+while not menu == "1" or not menu == "2":
+    if menu == "1":
         Person.donor_register_app()
-    elif int(menu) == 2:
+    elif menu == "2":
         Event.event_data()
     else:
         print("Please select a valid menu number!")
