cd3184b24bc49af4c9b3a21b2b66d0002a91b310
Bagoly Peter <djpetyaaa@gmail.com>
2015-11-23 13:35:00 +0100

    Change fejlesztes

---
+++ change_class.py
@@ -0,0 +1,35 @@
+__author__ = 'Péter'
+
+import csv
+from donor_object import DonorObject
+from event_object import DonationObject
+
+class ChangeSearch(object):
+   @staticmethod
+   def search_in_ids(id):
+      id = input("Search ID: ")
+      if id.isdigit():
+         file = open("Data/donations.csv", "r", encoding="utf-8")
+         reader = csv.reader(file)
+         for line in reader:
+            if id == line[0]:
+               original = DonationObject(line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9])
+               changed = DonationObject(line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9])
+               return line
+            else:
+               return "Not included in the database."
+      elif (id[:6].isdigit() and id[6:8].isalpha() and len(id) == 8) or (id[:6].isalpha() and id[6:8].isdigit() and len(id) == 8):
+         file = open("Data/donors.csv", "r", encoding="utf-8")
+         reader = csv.reader(file)
+         for line in reader:
+            if id == line[6]:
+               original = DonorObject(line[0],line[3],line[1],line[2],line[6],line[7],line[8],line[4],line[5],line[11],line[9],line[12])
+               changed = DonationObject(line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9])
+               return line
+            else:
+               return "Not included in the database."
+      else:
+         return ("The input is not correct.")
+
+
+print(ChangeSearch.search_in_ids(id))
--- event_object.py
+++ event_object.py
@@ -1,9 +1,7 @@
 class DonationObject(object):
 
-    def __init__(self, name, date_of_event, start_time, end_time, zip_code, city, address, available_beds, \
-                 planned_donor_number, event_duration_time, colon_in_duration_time, final_donor_number, \
-                 success_rate, success_text):
-        self.name = name
+    def __init__(self, date_of_event, start_time, end_time, zip_code, city, address, available_beds, \
+                 planned_donor_number, event_duration_time, colon_in_duration_time, final_donor_number):
         self.date_of_event = date_of_event
         self.start_time = start_time
         self.end_time = end_time
@@ -16,5 +14,3 @@ class DonationObject(object):
         self.event_duration_time = event_duration_time
         self.colon_in_duration_time = colon_in_duration_time
         self.final_donor_number = final_donor_number
-        self.success_rate = success_rate
-        self.success_text = success_text
