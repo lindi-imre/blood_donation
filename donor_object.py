8e28e64833f638fedf5bd0bd8cb70da08cfa6b8e
Kozma Balazs <kozma.balazs1992@gmail.com>
2015-11-23 11:33:47 +0100

    donor és event objektumok hozzaadasa

---
+++ donor_object.py
@@ -0,0 +1,18 @@
+class DonorObject(object):
+
+    def __init__(self, name, birth_date, weight, gender, uniqeid, expuniqeid, blood_type, last_donation, sick, \
+                 phone_number, email, age_of_donor, hemoglobin, suitable):
+      self.name = name
+      self.birth_date = birth_date
+      self.weight = weight
+      self.gender = gender
+      self.uniqeid = uniqeid
+      self.expuniqeid = expuniqeid
+      self.blood_type = blood_type
+      self.last_donation = last_donation
+      self.sick = sick
+      self.phone_number = phone_number
+      self.email = email
+      self.age_of_donor = age_of_donor
+      self.hemoglobin = hemoglobin
+      self.suitable = suitable
