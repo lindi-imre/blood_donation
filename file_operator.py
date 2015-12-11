6d9b67faab266e44ad8bfa9c464f255e7c9bb25c
Slezak Attila <attila.slezak@gmail.com>
2015-12-11 11:36:38 +0100

    Change is ready

--- file_operator.py
+++ file_operator.py
@@ -245,8 +245,10 @@ class FileOperator(object):
             table_name, id_name = '.Event', 'id'
         sql_command = "UPDATE " + database_name + table_name + " SET "
         for one_header, one_value in zip(id_and_header[1], change_val):
-            sql_command += one_header + " = '" + one_value + "' AND "
-        sql_command = sql_command[0:len(sql_command)-4] + "WHERE " + id_name + " = '" + id_and_header[0] + "';"
+            print(one_header)
+            # if one_header != 'id':
+            sql_command += one_header + " = '" + one_value + "', "
+        sql_command = sql_command[0:len(sql_command)-2] + " WHERE " + id_name + " = '" + id_and_header[0] + "';"
         print(sql_command)
         dbcon = mysql.connector.connect(user=user_name, password=user_password, host=server_name, database=database_name)
         cursor = dbcon.cursor()
